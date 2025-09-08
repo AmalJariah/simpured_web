8a58aae104c5152fb2858168c4fdaa85c4901929
AmalJariah <23FTT1865@student.pb.edu.bn>
2025-09-08 20:21:27 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Lara</title>
 
         <link rel="icon" href="{{ asset('favicon.ico') }}?v={{ time() }}" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
